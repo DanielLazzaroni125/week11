af7b924116805837546420f1a251b69b2608bd3e
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-11-15 14:54:40 -0800

    greet function

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+ function greet(user, time = "day"){
+    console.log(`Good ${time} ${user}`);
+    
+}
+
+greet("Daniel", "night");
+
+// const greet = function (user){
+//     console.log(`Hello ${user}`);
+// }
+
+// greet("Daniel");
+
+// const greet = (user, time) => {
+//     console.log(`Good ${time} ${user}`);
+// }
+
+// greet("Daniel");
